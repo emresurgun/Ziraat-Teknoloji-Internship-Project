dd301656878cf5990facabe7993a037289bc6854
Emre Şurğun <emresurgun1@gmail.com>
2025-08-20 04:29:49 +0300

    db password deleted from the source code

--- AppDbContextFactory.cs
+++ AppDbContextFactory.cs
@@ -9,7 +9,7 @@ namespace RestAPI
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=sqlserver;User Id=sa;Password=bfsjdJVFSOV8WvvjNKJfvds34135;Encrypt=False;TrustServerCertificate=True;MultipleActiveResultSets=true;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=sqlserver;User Id=sa;Password=;Encrypt=False;TrustServerCertificate=True;MultipleActiveResultSets=true;Connection Timeout=30;");
             return new AppDbContext(optionsBuilder.Options);
         }
     }
